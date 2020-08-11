749c5a184c35b14c99049f1539e3f3a443717cb9
Chandler L <cbl0015@auburn.edu>
2020-08-11 02:11:44 -0400

    Created initial files including .json files and installed npm's

---
+++ index.js
@@ -0,0 +1,19 @@
+// List out my NPM dependencies and require them
+var inquirer = require('inquirer');
+var fs = require('fs');
+var axios = require('axios');
+
+// I will need an inquirer .prompt function here
+
+// Followed by a .then promise that will fulfill the prompt
+// Incorporate the github API aspect inside this
+// Incorporate the axios dependancy here as well
+
+// inquirer .prompt function that will hold my object arrays
+    // types
+    // messages
+    // name
+
+// Then send everything to the readme via an fs.appendFile function
+    // Make sure to use ## tags to format the readme properly
+    // Use back tick notation with ${randomStuff} syntax
